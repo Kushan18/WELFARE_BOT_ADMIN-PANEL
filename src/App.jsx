e28edf8e2545c3b354bac0fd12e6e1acf06bf058
Kusha <kusha@gmail.com>
2026-07-05 16:46:32 +0530

    Fix dashboard centering layout issue

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,7 @@ import AdminDashboard from './components/AdminDashboard';
 import './App.css';
 
 function App() {
-  return (
-    <div style={{ height: '100vh', width: '100vw' }}>
-      <AdminDashboard />
-    </div>
-  );
+  return <AdminDashboard />;
 }
 
 export default App;
